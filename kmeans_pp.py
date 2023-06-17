ae7eec9b1e992d2092a9c5a005843323b74e38e0
MayaIw <mayaiwanir@mail.tau.ac.il>
2023-06-17 09:42:49 +0300

    committed 	modified:   kmeans_pp.py 	new file:   kmeansmodule.c 	setup.py

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -3,6 +3,7 @@ import pandas as pd
 import math
 import sys
 import copy
+import mykmeanssp as km
 
 #p1, p2 are arrays with float values, representing d dimentional points
 #the function calculates the euclidean distance between p1 and p2
@@ -14,25 +15,31 @@ def distance(p1, p2, d):
     return distance
 
 #x is a point in R^d, centroids is an array with k points in R^d
-def weight(x, centriods, d):
-   min_distance = distance(x, centriods[0], d)
-   for i in range(1, len(centriods)):
-       dist = distance(x, centriods[i], d)
+def weight(x, centroids, d):
+   min_distance = distance(x, centroids[0], d)
+   for i in range(1, len(centroids)):
+       dist = distance(x, centroids[i], d)
        if dist <= min_distance:
            min_distance = dist
    return min_distance
 
+def is_float(n):
+    try:
+        float(n)
+        return True
+    except:
+        return False
+
 def main():
     k=0
     iter=0
-    file=None
-    file_name=""
+    eps = 0.0
+    file_name_1=""
+    file_name_2=""
     elements = []
     centroids = []
     num_of_elements=0
     d=0
-    line_count =0
-    end=0
 
     if len(sys.argv) == 5:
         if sys.argv[1].isdigit():
@@ -42,8 +49,8 @@ def main():
             exit(0)
         iter = 200
         eps= float(sys.argv[2])
-        file_name_1 = sys.argv[3]
-        file_name_2 = sys.argv[4] 
+        file_name_1 = str(sys.argv[3])
+        file_name_2 = str(sys.argv[4]) 
     elif len(sys.argv) == 6:
          if sys.argv[1].isdigit():
             k = int(sys.argv[1])
@@ -68,15 +75,13 @@ def main():
     file1 = pd.read_csv(file_name_1)
     file2 = pd.read_csv(file_name_2)
 
-    file1.sample(5)
+    file = pd.merge(file1, file2,on=0)
+    file.sort_values(ascending=True)
+    elements = file.to_numpy()
 
-    '''file = open(file_name, "r")
     for line in file:
         point = [float(n) for n in line.split(',') if is_float(n)]
         elements.append(point)
-        line_count+=1
-        if line_count<=k:
-            centroids.append(copy.deepcopy(point))
 
     num_of_elements=len(elements)
     d=len(elements[0])
@@ -84,36 +89,15 @@ def main():
     if k<1 or k>num_of_elements:
         print("Invalid number of clusters!")
         exit(0)
+
+    np.random.seed(0)
+    centroids = copy.deepcopy(np.random.choice(elements))
+
+    for i in range(1,k):
+        weights = [weight(x, centroids, d) for x in elements]
+        centroids.append(copy.deepcopy(np.random.choice(elements, p=weights)))
     
-
-    clusters = [[0.0 for i in range(d)] for j in range(k)] 
-    size_of_clusters = [0 for i in range(k)]
-
-    assigned_centriod=0
-    for i in range(iter):
-        for j in range(num_of_elements):
-            assigned_centriod = clusterAssign(elements[j],centroids, k, d)
-            for l in range(d):
-                clusters[assigned_centriod][l] += elements[j][l]
-            size_of_clusters[assigned_centriod]+=1
-        centriodUpdate(clusters, size_of_clusters, k, d)
-        j=0
-        end=1
-        for j in range(k):
-            if distance(centroids[j], clusters[j], d)>=0.001:
-                end=0
-                break
-        if end:
-            break  
-        j=0
-        l=0
-        for j in range(k):
-            for l in range(d):
-                centroids[j][l] = float(clusters[j][l])
-                clusters[j][l] = 0.0
-            size_of_clusters[j] = 0
-
-    printClusters(centroids, d, k)
+    #print(km.fit(elements, centroids, k, iter, eps, num_of_elements, d))
 
 if __name__ == "__main__":
-    main()'''
+    main()
